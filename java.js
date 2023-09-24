e72dee3891fcd15f82f3380796974b6bebc35f57
muhammad45401 <saadjani3333@gmail.com>
2023-09-25 00:40:02 +0500

    java

--- java.js
+++ java.js
@@ -1,13 +1,6 @@
 alert("Error! please enter a valid password")
-alert("Welcome to JS land.../ Happy coding")
+alert("Welcome to JS land...\nHappy coding")
 alert("Welcome to JS land...")
-alert( "Happy coding/ alert prevent this page creating additional dialogs.")
+alert( "Happy coding\nprevent this page creating additional dialogs.")
 alert("Hello....I can run JS through  my web browser console")
-
-
-
-
-
-
-
 
